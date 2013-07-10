3ea9997b264f4ccb9631e3f832512bd5ae5d63b5
Matteo Casadei <matteo.casadei@gmail.com>
2013-07-10 07:47:45 +0000

    again modified it

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var content = fs.readFileSync('./index.html', 'utf8');
+var content = fs.readFileSync('./index.html', 'utf8');
 
 //console.log(content)
 app.get('/', function(request, response) {
